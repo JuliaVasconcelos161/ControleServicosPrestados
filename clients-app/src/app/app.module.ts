ea457e5434efec3d7c204325cf834820dd992199
Julia <juliavasconceloscr@gmail.com>
2022-12-14 23:06:06 -0300

    Separando a tela de login do layout home

--- clients-app/src/app/app.module.ts
+++ clients-app/src/app/app.module.ts
@@ -13,12 +13,14 @@ import { ProvidedServiceModule } from './provided-service/provided-service.modul
 import { ProvidedServiceService } from './provided-service.service';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { LayoutComponent } from './layout/layout.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    LayoutComponent
   ],
   imports: [
     FormsModule,
