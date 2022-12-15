5d398f64e1ab09eb0ce7607856bb38260284ad77
Julia <juliavasconceloscr@gmail.com>
2022-12-14 21:10:43 -0300

    Criando formulário de pesquisa

--- clients-app/src/app/provided-service/provided-service-list/provided-service-list.component.ts
+++ clients-app/src/app/provided-service/provided-service-list/provided-service-list.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-provided-service-list',
   templateUrl: './provided-service-list.component.html',
   styleUrls: ['./provided-service-list.component.css']
 })
-export class ProvidedServiceListComponent {
+export class ProvidedServiceListComponent implements OnInit{
+  name?: string;
+  month?: number;
+  months: number[];
 
+  constructor() {
+    this.months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+  }
+
+  ngOnInit(): void {
+      
+  }
+
+  searchService() {
+    console.log(this.name, this.month);
+  }
 }
